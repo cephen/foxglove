b038a6824c98ec829c455d72ad01b18d498e307c
Ash Villaschi <4luxxx@gmail.com>
2024-02-08 16:46:15 +0000

    Added a component to store input state

---
+++ Foxglove/Assets/Scripts/Input/InputState.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Foxglove.Input {
+    public struct InputState : IComponentData {
+        /// <summary>
+        /// Input move vector with a max length of 1
+        /// </summary>
+        public float2 Move;
+
+        /// <summary>
+        /// Tuple representation of aim direction and input device type.
+        /// when the bool is true, the float2 represents the mouse position in screen space
+        /// when false, the float2 represents the normalised joystick tilt direction
+        /// </summary>
+        public (float2, bool) Aim;
+
+        /// <summary>
+        /// Whether the attack button is pressed
+        /// </summary>
+        public bool Attack;
+    }
+}
