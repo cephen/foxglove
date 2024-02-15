d2c60bce5d99f4161f3edb5673916fbaeb27fe51
Ash Villaschi <4luxxx@gmail.com>
2024-02-15 15:28:18 +0000

    comments are cool sometimes

--- Foxglove/Assets/Scripts/Input/InputState.cs
+++ Foxglove/Assets/Scripts/Input/InputState.cs
@@ -21,7 +21,12 @@ namespace Foxglove.Input {
     }
 
     public struct AimState {
+        /// <summary>
+        /// IsMouseAim true: mouse position delta
+        /// IsMouseAim false: right stick tilt, normalized
+        /// </summary>
         public float2 Value;
+
         public bool IsMouseAim;
     }
 }
