d663bb0ff3e297ce34a3a590db1cfc8894a5c181
Ash Villaschi <4luxxx@gmail.com>
2024-02-15 16:04:48 +0000

    Add an authoring script for the player

---
+++ Foxglove/Assets/Scripts/Characters/PlayerAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Foxglove.Motion;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Foxglove.Characters {
+    public class PlayerAuthoring : MonoBehaviour {
+        public MotionSettings MotionSettings;
+
+        public class PlayerBaker : Baker<PlayerAuthoring> {
+            /// <inheritdoc />
+            public override void Bake(PlayerAuthoring authoring) {
+                Entity player = GetEntity(authoring.gameObject,
+                    TransformUsageFlags.Dynamic | TransformUsageFlags.WorldSpace);
+
+                AddComponent<PlayerTag>(player);
+                AddComponent(player, authoring.MotionSettings);
+                AddComponent<TargetVelocity>(player);
+            }
+        }
+    }
+
+    public struct PlayerTag : IComponentData { }
+}
