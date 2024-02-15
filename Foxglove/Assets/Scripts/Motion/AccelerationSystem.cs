21f66201045026baef67e6a1c1fa437b63498baa
Ash Villaschi <4luxxx@gmail.com>
2024-02-15 16:40:19 +0000

    First draft of acceleration system

---
+++ Foxglove/Assets/Scripts/Motion/AccelerationSystem.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+namespace Foxglove.Motion {
+    [BurstCompile]
+    [UpdateInGroup(typeof(PhysicsInitializeGroup))]
+    public partial struct AccelerationSystem : ISystem {
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate<MotionSettings>();
+            state.RequireForUpdate<TargetVelocity>();
+            state.RequireForUpdate<PhysicsVelocity>();
+        }
+
+        public void OnDestroy(ref SystemState state) { }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) {
+            float deltaTime = SystemAPI.Time.fixedDeltaTime;
+            foreach (PhysicsCharacterAspect character in SystemAPI.Query<PhysicsCharacterAspect>()) {
+                ref PhysicsVelocity current = ref character.PhysicsVelocity.ValueRW;
+                float3 targetVelocity = character.TargetVelocity.ValueRO.Value;
+                float3 newVelocity = current.Linear
+                                     + targetVelocity * character.MotionSettings.ValueRO.AccelerationRate * deltaTime;
+
+                float3 direction = math.normalizesafe(newVelocity);
+                float speed = math.min(character.MotionSettings.ValueRO.MaxHorizontalSpeed, math.length(newVelocity));
+                float3 finalVelocity = speed < math.EPSILON ? float3.zero : direction * speed;
+                current.Linear = finalVelocity;
+            }
+        }
+    }
+}
