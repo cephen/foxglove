17a7c2a42eba50cef83b1ef3bc161726b8de94d1
Ash Villaschi <fizix@sidefx.dev>
2024-02-20 19:59:12 +0000

    Add camera authoring components to player

--- Foxglove/Assets/Scripts/Characters/PlayerAuthoring.cs
+++ Foxglove/Assets/Scripts/Characters/PlayerAuthoring.cs
@@ -1,10 +1,17 @@
-﻿using Foxglove.Motion;
+using Foxglove.Camera;
+using Foxglove.Motion;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Foxglove.Characters {
     public class PlayerAuthoring : MonoBehaviour {
         public MotionSettings MotionSettings;
+
+        [Header("Camera")]
+        public float3 CameraOffset;
+
+        public float CameraDistance = 1f;
 
         public class PlayerBaker : Baker<PlayerAuthoring> {
             /// <inheritdoc />
@@ -13,8 +20,12 @@ namespace Foxglove.Characters {
                     TransformUsageFlags.Dynamic | TransformUsageFlags.WorldSpace);
 
                 AddComponent<PlayerTag>(player);
-                AddComponent(player, authoring.MotionSettings);
-                AddComponent<TargetVelocity>(player);
+                // AddComponent<TargetVelocity>(player);
+                // AddComponent(player, authoring.MotionSettings);
+                AddComponent(player, new CameraPosition());
+                AddComponent(player, new CameraTarget());
+                AddComponent(player, new CameraOffset { Value = authoring.CameraOffset });
+                AddComponent(player, new CameraDistance { Value = authoring.CameraDistance });
             }
         }
     }
