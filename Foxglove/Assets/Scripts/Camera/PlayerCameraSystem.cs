1d50a26c5e36c80761f9779840b815c6f4f3de3a
Ash Villaschi <fizix@sidefx.dev>
2024-02-20 20:00:16 +0000

    Sync camera simulated position to transform

---
+++ Foxglove/Assets/Scripts/Camera/PlayerCameraSystem.cs
@@ -0,0 +1,28 @@
+﻿using Foxglove.Characters;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Foxglove.Camera {
+    [BurstCompile]
+    public partial class PlayerCameraSystem : SystemBase {
+        protected override void OnCreate() {
+            RequireForUpdate<PlayerTag>();
+            RequireForUpdate<CameraPosition>();
+        }
+
+        [BurstCompile]
+        protected override void OnUpdate() {
+            if (UnityEngine.Camera.main == null)
+                // Log Error
+                return;
+
+            UnityEngine.Camera camera = UnityEngine.Camera.main;
+            Entity player = SystemAPI.GetSingletonEntity<PlayerTag>();
+            var cameraPosition = SystemAPI.GetComponent<CameraPosition>(player);
+            var cameraTarget = SystemAPI.GetComponent<CameraTarget>(player);
+            camera.transform.position = cameraPosition.Value;
+            camera.transform.rotation = quaternion.LookRotation(cameraTarget.Value - cameraPosition.Value, math.up());
+        }
+    }
+}
