9d5e78b053bb282723507ed7c537fa0e3082c7c5
Ash Villaschi <4luxxx@gmail.com>
2024-02-21 15:45:29 +0000

    Remove unused aspect

--- Foxglove/Assets/Scripts/Motion/MotionComponents.cs
+++ Foxglove/Assets/Scripts/Motion/MotionComponents.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Physics;
-using Unity.Transforms;
 
 namespace Foxglove.Motion {
     [Serializable]
@@ -13,13 +11,5 @@ namespace Foxglove.Motion {
 
     public struct TargetVelocity : IComponentData {
         public float3 Value;
-    }
-
-    public readonly partial struct PhysicsCharacterAspect : IAspect {
-        public readonly Entity Entity;
-        public readonly RefRO<MotionSettings> MotionSettings;
-        public readonly RefRO<LocalTransform> LocalTransform;
-        public readonly RefRW<PhysicsVelocity> PhysicsVelocity;
-        public readonly RefRW<TargetVelocity> TargetVelocity;
     }
 }
