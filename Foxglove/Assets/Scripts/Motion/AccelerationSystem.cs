5cd9caba8304db0b98aafe200230238eef06cf43
Ash Villaschi <4luxxx@gmail.com>
2024-02-21 15:48:57 +0000

    Move acceleration system to main thread

--- Foxglove/Assets/Scripts/Motion/AccelerationSystem.cs
+++ Foxglove/Assets/Scripts/Motion/AccelerationSystem.cs
@@ -6,30 +6,31 @@ using Unity.Physics.Systems;
 
 namespace Foxglove.Motion {
     [BurstCompile]
+    [UpdateAfter(typeof(PlayerOrientationSystem))]
     [UpdateInGroup(typeof(PhysicsInitializeGroup))]
-    public partial struct AccelerationSystem : ISystem {
-        public void OnCreate(ref SystemState state) {
-            state.RequireForUpdate<MotionSettings>();
-            state.RequireForUpdate<TargetVelocity>();
-            state.RequireForUpdate<PhysicsVelocity>();
+    public partial class AccelerationSystem : SystemBase {
+        protected override void OnCreate() {
+            RequireForUpdate<MotionSettings>();
+            RequireForUpdate<TargetVelocity>();
+            RequireForUpdate<PhysicsVelocity>();
         }
 
-        public void OnDestroy(ref SystemState state) { }
+        protected override void OnDestroy() { }
 
         [BurstCompile]
-        public void OnUpdate(ref SystemState state) {
-            float deltaTime = SystemAPI.Time.fixedDeltaTime;
-            foreach (PhysicsCharacterAspect character in SystemAPI.Query<PhysicsCharacterAspect>()) {
-                ref PhysicsVelocity current = ref character.PhysicsVelocity.ValueRW;
-                float3 targetVelocity = character.TargetVelocity.ValueRO.Value;
-                float3 newVelocity = current.Linear
-                                     + targetVelocity * character.MotionSettings.ValueRO.AccelerationRate * deltaTime;
-
-                float3 direction = math.normalizesafe(newVelocity);
-                float speed = math.min(character.MotionSettings.ValueRO.MaxHorizontalSpeed, math.length(newVelocity));
-                float3 finalVelocity = speed < math.EPSILON ? float3.zero : direction * speed;
-                current.Linear = finalVelocity;
-            }
+        protected override void OnUpdate() {
+            Entities
+                .ForEach((ref PhysicsVelocity currentVelocity,
+                    in TargetVelocity targetVelocity, in MotionSettings settings) => {
+                    float deltaTime = SystemAPI.Time.fixedDeltaTime;
+                    float3 accelerated = currentVelocity.Linear
+                                         + targetVelocity.Value * settings.AccelerationRate * deltaTime;
+                    float3 direction = math.normalizesafe(accelerated);
+                    float speed = math.min(math.length(accelerated), settings.MaxHorizontalSpeed);
+                    currentVelocity.Linear = speed > math.EPSILON ? direction * speed : float3.zero;
+                    currentVelocity.Angular = float3.zero;
+                })
+                .Run();
         }
     }
 }
