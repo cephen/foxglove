4ab50c9c55cc8cc6551076f8e0889faf8a62edbb
Ash Villaschi <4luxxx@gmail.com>
2024-02-21 15:45:19 +0000

    Fix Orientation of characters

---
+++ Foxglove/Assets/Scripts/Motion/CharacterOrientationSystem.cs
@@ -0,0 +1,26 @@
+﻿using Foxglove.Characters;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Foxglove.Motion {
+    [BurstCompile]
+    public partial class CharacterOrientationSystem : SystemBase {
+        protected override void OnCreate() {
+            RequireForUpdate<CharacterTag>();
+            RequireForUpdate<Heading>();
+            RequireForUpdate<LocalTransform>();
+        }
+
+        [BurstCompile]
+        protected override void OnUpdate() {
+            Entities
+                .WithAll<CharacterTag>()
+                .ForEach((ref LocalTransform transform, in Heading heading) => {
+                    transform.Rotation = quaternion.RotateY(heading.Radians);
+                })
+                .Run();
+        }
+    }
+}
