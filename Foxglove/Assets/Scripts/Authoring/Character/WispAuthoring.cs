5e7698f06f18626969d583522411d8094512ebac
Ash Villaschi <4luxxx@gmail.com>
2024-03-06 17:02:07 +0000

    Added a wisp authoring component

---
+++ Foxglove/Assets/Scripts/Authoring/Character/WispAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Foxglove.Character {
+    public struct WispTag : IComponentData { }
+
+    public sealed class WispAuthoring : MonoBehaviour {
+        public sealed class Baker : Baker<WispAuthoring> {
+            public override void Bake(WispAuthoring authoring) {
+                Entity wisp = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<WispTag>(wisp);
+            }
+        }
+    }
+}
