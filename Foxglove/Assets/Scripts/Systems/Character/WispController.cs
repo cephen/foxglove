97bbeb84ca34327e3f9485507b229f17c15afa0d
Ash Villaschi <4luxxx@gmail.com>
2024-03-06 17:07:40 +0000

    Added a simple wisp controller to test flight

---
+++ Foxglove/Assets/Scripts/Systems/Character/WispController.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace Foxglove.Character {
+    [BurstCompile]
+    public partial struct WispController : ISystem {
+        /*
+         * TODO: Identify player location
+         * TODO: Path towards player
+         * TODO: Flow Field angle?
+         */
+        public void OnCreate(ref SystemState state) {
+            state.RequireForUpdate(SystemAPI.QueryBuilder().WithAll<FoxgloveCharacterControl, WispTag>().Build());
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state) {
+            foreach (RefRW<FoxgloveCharacterControl> control
+                in SystemAPI.Query<RefRW<FoxgloveCharacterControl>>().WithAll<WispTag>())
+                control.ValueRW.MoveVector.y = 1f;
+        }
+
+        public void OnDestroy(ref SystemState state) { }
+    }
+}
