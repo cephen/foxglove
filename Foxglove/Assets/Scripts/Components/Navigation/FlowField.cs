720a9a20c0997ae185562201834f4f3b88e764de
Ash Villaschi <4luxxx@gmail.com>
2024-03-07 16:58:56 +0000

    clean up flow field components

--- Foxglove/Assets/Scripts/Components/Navigation/FlowField.cs
+++ Foxglove/Assets/Scripts/Components/Navigation/FlowField.cs
@@ -4,44 +4,19 @@ using Unity.Transforms;
 
 namespace Foxglove.Navigation {
     public struct FlowFieldTarget : IComponentData {
-        public Entity Value;
+        public Entity TargetEntity;
+        public uint3 TargetCoordinate;
     }
 
-    public struct FlowFieldResolution : IComponentData {
-        public int3 Value;
-    }
-
-    public struct FlowFieldChunk : IComponentData {
-        public Entity Parent;
-
-        /// <summary>
-        /// Position of the chunk in grid coordinates relative to the chunk origin.
-        /// Each component is in the range [-1, 1]
-        /// </summary>
-        public int3 Position;
-
-        public float3 Size;
+    public struct FlowFieldSample : IBufferElementData {
+        public float Cost;
+        public float3 Direction;
     }
 
     public readonly partial struct FlowFieldAspect : IAspect {
         public readonly Entity Entity;
         public readonly RefRW<FlowFieldTarget> Target;
-    }
-
-    public readonly partial struct FlowFieldChunkAspect : IAspect {
-        public readonly Entity Entity;
-        public readonly RefRW<FlowFieldChunk> Chunk;
-        public readonly RefRW<FlowFieldResolution> Resolution;
-        public readonly RefRW<LocalTransform> Transform;
+        public readonly RefRW<LocalToWorld> Origin;
         public readonly DynamicBuffer<FlowFieldSample> Samples;
-
-        public bool Contains(float3 position) => true;
-    }
-
-    public struct FlowFieldSample : IBufferElementData {
-        public Entity Chunk;
-        public int3 Position;
-        public float Cost;
-        public float3 Direction;
     }
 }
