790fb90fce1814c302599c72fd096f7bcc4b6bbd
Ash Villaschi <4luxxx@gmail.com>
2024-03-13 10:15:46 +0000

    Change WispAgentSystem queries

--- Foxglove/Assets/Scripts/Systems/Agent/WispAgentSystem.cs
+++ Foxglove/Assets/Scripts/Systems/Agent/WispAgentSystem.cs
@@ -1,18 +1,22 @@
 using Foxglove.Character;
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Transforms;
 
 namespace Foxglove.Agent {
     [BurstCompile]
     [UpdateInGroup(typeof(AgentUpdateGroup))]
     public partial struct WispAgentSystem : ISystem {
         public void OnCreate(ref SystemState state) {
+            // Agent components
             state.RequireForUpdate(
                 SystemAPI
                     .QueryBuilder()
-                    .WithAll<WispTag, CharacterController, Blackboard>()
+                    .WithAll<WispTag, CharacterController, LocalToWorld>()
                     .Build()
             );
+
+            state.RequireForUpdate<Blackboard>();
         }
 
         public void OnDestroy(ref SystemState state) { }
