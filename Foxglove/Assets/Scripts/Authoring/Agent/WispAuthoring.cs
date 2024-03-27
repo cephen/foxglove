09312cc1db6cbf4873db674ba00fa22db64b6285
Ash Villaschi <4luxxx@gmail.com>
2024-03-27 17:05:26 +0000

    Add health component

--- Foxglove/Assets/Scripts/Authoring/Agent/WispAuthoring.cs
+++ Foxglove/Assets/Scripts/Authoring/Agent/WispAuthoring.cs
@@ -1,4 +1,4 @@
-﻿using Foxglove.Agent;
+using Foxglove.Agent;
 using Unity.Entities;
 using UnityEngine;
 
@@ -7,11 +7,22 @@ namespace Foxglove.Authoring.Agent {
     /// Added to the wisp prefab, when instantiated this component is converted to a <see cref="WispTag" />
     /// </summary>
     public sealed class WispAuthoring : MonoBehaviour {
+        public int MaxHealth = 100;
+
         public sealed class Baker : Baker<WispAuthoring> {
             public override void Bake(WispAuthoring authoring) {
                 Entity wisp = GetEntity(TransformUsageFlags.Dynamic);
                 AddComponent<WispTag>(wisp);
                 AddComponent(wisp, WispState.Default());
+                AddComponent(
+                    wisp,
+                    new Health {
+                        Max = authoring.MaxHealth,
+                        Current = authoring.MaxHealth,
+                    }
+                );
+                AddComponent<DespawnTimer>(wisp);
+                SetComponentEnabled<DespawnTimer>(wisp, false);
             }
         }
     }
---
+++ Foxglove/Assets/Scripts/Components/Character/Health.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace Foxglove.Character {
+    public struct Health : IComponentData {
+        public float Current;
+        public float Max;
+
+        public Health(float max) {
+            Max = max;
+            Current = max;
+        }
+    }
+}
