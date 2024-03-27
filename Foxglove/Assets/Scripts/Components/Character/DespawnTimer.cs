b038209299147e9fa38c19e8511ba70ca40d5075
Ash Villaschi <4luxxx@gmail.com>
2024-03-27 17:04:15 +0000

    Add DespawnTimer component

---
+++ Foxglove/Assets/Scripts/Components/Character/DespawnTimer.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+namespace Foxglove.Character {
+    public struct DespawnTimer : IComponentData, IEnableableComponent {
+        public uint TickToDestroy;
+        public DespawnTimer(uint tickToDestroy) => TickToDestroy = tickToDestroy;
+    }
+}
