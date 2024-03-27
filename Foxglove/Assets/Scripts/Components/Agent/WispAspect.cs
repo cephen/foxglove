6862d4aa42ee0d0eb5104911a612b282cb2c684d
Ash Villaschi <4luxxx@gmail.com>
2024-03-27 15:14:06 +0000

    Add WispAspect

---
+++ Foxglove/Assets/Scripts/Components/Agent/WispAspect.cs
@@ -0,0 +1,19 @@
+﻿using Foxglove.Character;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace Foxglove.Agent {
+    [BurstCompile]
+    public readonly partial struct WispAspect : IAspect {
+        public readonly Entity Entity;
+        public readonly RefRW<LocalToWorld> LocalToWorld;
+        public readonly RefRW<CharacterController> CharacterController;
+        public readonly RefRW<WispState> State;
+        public readonly RefRW<Health> Health;
+
+        public readonly RefRO<CharacterSettings> CharacterSettings;
+
+        private readonly RefRO<WispTag> _wispTag;
+    }
+}
