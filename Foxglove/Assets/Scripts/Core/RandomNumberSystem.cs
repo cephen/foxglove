afcf10a34f2c113183690e2dfd14d0504729670f
Ash Villaschi <4luxxx@gmail.com>
2024-03-28 15:55:43 +0000

    Add an RNG system

---
+++ Foxglove/Assets/Scripts/Core/RandomNumberSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity.Burst;
+using Unity.Entities;
+using Random = Unity.Mathematics.Random;
+
+namespace Foxglove {
+    [BurstCompile]
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public partial struct RandomNumberSystem : ISystem {
+        public struct Singleton : IComponentData {
+            public Random Random;
+        }
+
+        public void OnCreate(ref SystemState state) {
+            state.EntityManager.CreateOrSetSingleton(
+                new Singleton {
+                    Random = new Random((uint)DateTimeOffset.UtcNow.GetHashCode()),
+                }
+            );
+        }
+
+        public void OnDestroy(ref SystemState state) { }
+        public void OnUpdate(ref SystemState state) { }
+    }
+}
