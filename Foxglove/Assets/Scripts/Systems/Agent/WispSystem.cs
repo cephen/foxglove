0f4bc58dfda9198d82bd34a6972d3a02b9798563
Ash Villaschi <4luxxx@gmail.com>
2024-03-28 15:24:55 +0000

    Move wisp state machine logic into worker thread job

--- Foxglove/Assets/Scripts/Systems/Agent/WispSystem.cs
+++ Foxglove/Assets/Scripts/Systems/Agent/WispSystem.cs
@@ -1,6 +1,7 @@
 using System;
 using Foxglove.Character;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Logging;
 using Unity.Mathematics;
@@ -25,72 +26,98 @@ namespace Foxglove.Agent {
 
             uint tick = SystemAPI.GetSingleton<FixedTickSystem.State>().Tick;
 
-            EntityCommandBuffer ecb = SystemAPI
+            EntityCommandBuffer commands = SystemAPI
                 .GetSingleton<EndFixedStepSimulationEntityCommandBufferSystem.Singleton>()
                 .CreateCommandBuffer(state.WorldUnmanaged);
 
-            foreach (WispAspect aspect in SystemAPI
-                .Query<WispAspect>()
-                .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)) {
+            state.Dependency = new WispStateMachineJob {
+                Tick = tick,
+                Blackboard = blackboard,
+                Commands = commands.AsParallelWriter(),
+            }.ScheduleParallel( /*wispQuery,*/ state.Dependency);
+        }
+
+        [BurstCompile]
+        [WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)]
+        private partial struct WispStateMachineJob : IJobEntity {
+            public uint Tick;
+            public Blackboard Blackboard;
+            public EntityCommandBuffer.ParallelWriter Commands;
+
+            public void Execute(Entity entity, WispAspect aspect, [ChunkIndexInQuery] int chunkIndex) {
+                // regular ToString is disallowed in burst because it allocates on the heap
+                FixedString64Bytes entityDebugName = entity.ToFixedString();
+
                 ref WispState wispState = ref aspect.State.ValueRW;
 
-                if (aspect.Health.ValueRO.Current <= 0f)
-                    wispState.Current = WispState.State.Die;
+                if (aspect.Health.ValueRO.Current <= 0
+                    && aspect.State.ValueRO.Current is not WispState.State.Die)
+                    wispState.TransitionTo(WispState.State.Die);
 
                 switch (wispState.Current) {
                     case WispState.State.Inactive:
-                        // Only used on freshly spawned wisps
-                        // No associated behaviour
-                        Log.Warning("Wisp {wisp} should not be inactive but is", aspect.Entity);
+                        // Freshly spawned wisps have state {current = Spawn, previous = Inactive}
+                        // Inactive is not used anywhere else, so this branch should never be entered
+                        Log.Error("Wisp {entity} should not be inactive but is", entityDebugName);
                         break;
                     case WispState.State.Spawn:
+                        Log.Debug("Spawning Wisp {entity}", entityDebugName);
                         // Set default stats
                         aspect.Health.ValueRW.Max = 100;
                         aspect.Health.ValueRW.Current = 100;
                         // Enable Character Controller
-                        ecb.SetComponentEnabled<CharacterController>(aspect.Entity, true);
+                        Commands.SetComponentEnabled<CharacterController>(chunkIndex, aspect.Entity, true);
                         // Transition to Patrol state
-                        wispState.Current = WispState.State.Patrol;
-                        wispState.Previous = WispState.State.Spawn;
+                        wispState.TransitionTo(WispState.State.Patrol);
                         break;
                     case WispState.State.Patrol:
                         // If in range of player transition to attack
                         float distanceToPlayer = math.distance(
                             aspect.LocalToWorld.ValueRO.Position,
-                            blackboard.PlayerPosition
+                            Blackboard.PlayerPosition
                         );
 
-                        if (distanceToPlayer < 10f) {
-                            Log.Debug("Wisp {wisp} attacking player", aspect.Entity);
-                            wispState.Current = WispState.State.Attack;
-                            wispState.Previous = WispState.State.Patrol;
+                        // TODO: Add line of sight check
+                        // TODO: add attack cooldown
+                        if (distanceToPlayer <= 10) {
+                            Log.Debug("Wisp {entity} transitioning to Attack State", entityDebugName);
+                            wispState.TransitionTo(WispState.State.Attack);
                         }
 
                         break;
                     case WispState.State.Attack:
-                        // Spawn projectile
-                        Log.Debug("Wisp {wisp} spawning projectile", aspect.Entity);
-                        // Transition to Patrol
-                        wispState.Current = WispState.State.Patrol;
-                        wispState.Previous = WispState.State.Attack;
+                        Log.Debug("Wisp {wisp} attacking player", entityDebugName);
+                        // TODO: spawn projectile
+                        wispState.TransitionTo(WispState.State.Patrol);
                         break;
                     case WispState.State.Die:
-                        // Disable character controller
-                        if (SystemAPI.IsComponentEnabled<CharacterController>(aspect.Entity)) {
+                        if (aspect.IsCharacterControllerEnabled.ValueRO) {
+                            Log.Debug(
+                                "Wisp {entity} died, disabling character controller and adding despawn timer",
+                                entityDebugName
+                            );
                             // Disable character controller
-                            ecb.SetComponentEnabled<CharacterController>(aspect.Entity, false);
+                            Commands.SetComponentEnabled<CharacterController>(chunkIndex, aspect.Entity, false);
                             // Despawn in one second (50 ticks per second)
-                            ecb.SetComponentEnabled<DespawnTimer>(aspect.Entity, true);
-                            ecb.SetComponent(aspect.Entity, new DespawnTimer(tick + 50));
+                            Commands.SetComponentEnabled<DespawnTimer>(chunkIndex, aspect.Entity, true);
+                            Commands.SetComponent(chunkIndex, aspect.Entity, new DespawnTimer(Tick + 50));
+                        }
+                        else if (aspect.DespawnTimer.ValueRO.TickToDestroy <= Tick) {
+                            // Transition to Despawn after 1 second
+                            Log.Debug("Despawning Wisp {entity}", entityDebugName);
+                            wispState.TransitionTo(WispState.State.Despawn);
                         }
 
-                        // Transition to Despawn after 1 second
                         break;
                     case WispState.State.Despawn:
-                        ecb.DestroyEntity(aspect.Entity); // Clean up entity
+                        Commands.DestroyEntity(chunkIndex, aspect.Entity); // Clean up entity
                         break;
                     default:
-                        Log.Error("Wisp {wisp} is in invalid state {state}", aspect.Entity, wispState.Current);
+                        Log.Error(
+                            "Wisp {wisp} is in invalid state {state}",
+                            entityDebugName,
+                            nameof(wispState.Current)
+                        );
                         throw new ArgumentOutOfRangeException();
                 }
             }
