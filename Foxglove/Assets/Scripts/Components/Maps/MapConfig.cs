e98e58ee512ddbbd5939254b6c021bfd82c7c8ec
Ash Villaschi <fizix@sidefx.dev>
2024-04-03 00:42:19 +0100

    Add Map builder config

---
+++ Foxglove/Assets/Scripts/Components/Maps/MapConfig.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+
+namespace Foxglove.Maps {
+    public struct MapConfig {
+        public Entity MapRoot;
+        public uint Seed;
+        public int RoomsToGenerate;
+        public int MinRoomSize;
+        public int MaxRoomSize;
+        public int Radius;
+        public readonly int Diameter => Radius * 2;
+    }
+}
