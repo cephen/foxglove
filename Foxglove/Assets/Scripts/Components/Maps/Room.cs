0f3981f0ca45e9b7b5395ba423d79100af027349
Ash Villaschi <4luxxx@gmail.com>
2024-04-03 11:17:07 +0100

    Fix room center calculation

--- Foxglove/Assets/Scripts/Components/Maps/Room.cs
+++ Foxglove/Assets/Scripts/Components/Maps/Room.cs
@@ -6,6 +6,6 @@ namespace Foxglove.Maps {
         public int2 Position;
         public int2 Size;
 
-        public readonly float2 Center => (float2)(Position + Size) / 2;
+        public readonly float2 Center => Position + Size / 2;
     }
 }
