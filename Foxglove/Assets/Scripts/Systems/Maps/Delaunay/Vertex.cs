e0f6a6164b5e08fa6f65122d7e2f81ed04763125
Ash Villaschi <fizix@sidefx.dev>
2024-04-03 06:48:56 +0100

    Add vertex struct

---
+++ Foxglove/Assets/Scripts/Systems/Maps/Delaunay/Vertex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Mathematics;
+
+namespace Foxglove.Maps.Graphs {
+    public struct Vertex : IEquatable<Vertex> {
+        public float2 Position;
+
+        public static implicit operator float2(Vertex v) => v.Position;
+        public static implicit operator Vertex(float2 v) => new() { Position = v };
+
+        public bool Equals(Vertex other) => Position.Equals(other.Position);
+        public override bool Equals(object obj) => obj is Vertex other && Equals(other);
+
+        public readonly override int GetHashCode() => Position.GetHashCode();
+    }
+}
