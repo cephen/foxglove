959f89ca92a87f954610ff0e4801cd5a5ab29873
Ash Villaschi <fizix@sidefx.dev>
2024-04-03 02:54:12 +0100

    Add component for triggering map generation

---
+++ Foxglove/Assets/Scripts/Components/Maps/ShouldGenerateMap.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Foxglove.Maps {
+    public struct ShouldGenerateMap : IComponentData, IEnableableComponent {
+        public MapConfig Config;
+        public static implicit operator MapConfig(ShouldGenerateMap request) => request.Config;
+        public static implicit operator ShouldGenerateMap(MapConfig config) => new() { Config = config };
+    }
+}
