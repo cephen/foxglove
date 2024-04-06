f8e2ec876ce7df818db43b597c6199c83bcf7450
Ash Villaschi <fizix@sidefx.dev>
2024-04-06 01:03:47 +0100

    fuckin americans

--- Foxglove/Assets/Scripts/Systems/Maps/Editor/DrawEdgeDebugLinesJob.cs
+++ Foxglove/Assets/Scripts/Systems/Maps/Editor/DrawEdgeDebugLinesJob.cs
@@ -8,14 +8,14 @@ namespace Foxglove.Maps.Editor {
     [BurstCompile]
     internal partial struct DrawEdgeDebugLinesJob : IJobEntity {
         public float DeltaTime;
-        public Color Colour;
+        public Color Color;
 
         [BurstCompile]
         private readonly void Execute(in DynamicBuffer<Edge> edges) {
             foreach (Edge edge in edges) {
                 float3 start = new(edge.A.Position.x, 0, edge.A.Position.y);
                 float3 end = new(edge.B.Position.x, 0, edge.B.Position.y);
-                Debug.DrawLine(start, end, Colour, DeltaTime);
+                Debug.DrawLine(start, end, Color, DeltaTime);
             }
         }
     }
