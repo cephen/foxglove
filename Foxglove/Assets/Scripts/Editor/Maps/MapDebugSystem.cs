9374f1c62c39f0b671df06fb0f2b3a5b1da28c23
Ash Villaschi <4luxxx@gmail.com>
2024-04-12 01:40:33 +0100

    remove idle debugging

--- Foxglove/Assets/Scripts/Editor/Maps/MapDebugSystem.cs
+++ Foxglove/Assets/Scripts/Editor/Maps/MapDebugSystem.cs
@@ -25,36 +25,20 @@ namespace Foxglove.Editor.Maps {
         /// If no entities are matched by the query, the jobs won't run.
         /// </summary>
         [BurstCompile]
-        readonly void ISystem.OnUpdate(ref SystemState ecs) {
-            SystemState mapGenSystem = ecs.WorldUnmanaged.GetExistingSystemState<MapGeneratorSystem>();
-            var genState = SystemAPI.GetComponent<State<GeneratorState>>(mapGenSystem.SystemHandle);
-            Entity mapRoot = SystemAPI.GetSingletonEntity<Map>();
+        void ISystem.OnUpdate(ref SystemState ecs) {
+            JobHandle drawRooms = new DrawRoomDebugLinesJob {
+                DrawTime = CheckpointUpdateGroup.UPDATE_RATE,
+                Color = Color.yellow,
+            }.Schedule(_mapQuery, ecs.Dependency);
 
-            switch (genState.Current) {
-                case GeneratorState.Idle:
-                    // When idle, the map either doesn't exist or is fully generated.
-                    DynamicBuffer<MapCell> cells = SystemAPI.GetBuffer<MapCell>(mapRoot);
-                    ecs.Dependency = new DrawCellDebugLinesJob {
-                        Cells = cells.AsNativeArray().AsReadOnly(),
-                        Config = SystemAPI.GetComponent<MapConfig>(mapRoot),
-                        DeltaTime = CheckpointUpdateGroup.UPDATE_RATE,
-                    }.Schedule(cells.Length, 1024, ecs.Dependency);
+            JobHandle drawEdges = new DrawEdgeDebugLinesJob {
+                DeltaTime = CheckpointUpdateGroup.UPDATE_RATE,
+                Color = Color.red,
+            }.Schedule(_mapQuery, ecs.Dependency);
 
-                    break;
-                default:
-                    JobHandle drawRooms = new DrawRoomDebugLinesJob {
-                        DrawTime = CheckpointUpdateGroup.UPDATE_RATE,
-                        Color = Color.yellow,
-                    }.Schedule(_mapQuery, ecs.Dependency);
+            ecs.Dependency = JobHandle.CombineDependencies(drawRooms, drawEdges);
+            return;
 
-                    JobHandle drawEdges = new DrawEdgeDebugLinesJob {
-                        DeltaTime = CheckpointUpdateGroup.UPDATE_RATE,
-                        Color = Color.red,
-                    }.Schedule(_mapQuery, ecs.Dependency);
-
-                    ecs.Dependency = JobHandle.CombineDependencies(drawRooms, drawEdges);
-                    return;
-            }
         }
 
         [BurstCompile]
