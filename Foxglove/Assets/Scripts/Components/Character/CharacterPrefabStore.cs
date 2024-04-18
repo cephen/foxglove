dd0bb3903d12d1b5e6b558d2c4159e594db52f79
Ash Villaschi <4luxxx@gmail.com>
2024-04-18 18:24:12 +0100

    Add a character prefab store component

---
+++ Foxglove/Assets/Scripts/Components/Character/CharacterPrefabStore.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+namespace Foxglove.Character {
+    public struct CharacterPrefabStore : IComponentData {
+        public Entity PlayerPrefab { get; init; }
+        public Entity WispPrefab { get; init; }
+    }
+}
