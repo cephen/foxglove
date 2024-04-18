919d180515de006b96dd20a8d0755b9f7a5668b8
Ash Villaschi <4luxxx@gmail.com>
2024-04-18 19:25:41 +0100

    move prefab store to Gameplay namespace

--- Foxglove/Assets/Scripts/Components/Character/CharacterPrefabStore.cs
+++
@@ -1,8 +0,0 @@
-﻿using Unity.Entities;
-
-namespace Foxglove.Character {
-    public struct CharacterPrefabStore : IComponentData {
-        public Entity PlayerPrefab { get; init; }
-        public Entity WispPrefab { get; init; }
-    }
-}
