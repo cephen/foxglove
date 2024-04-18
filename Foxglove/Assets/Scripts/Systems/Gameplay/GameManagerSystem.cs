ebc736816a0356094d021dd2e5f3fe9d1ea21a91
Ash Villaschi <4luxxx@gmail.com>
2024-04-18 18:41:50 +0100

    Spawn the player when the map is ready

--- Foxglove/Assets/Scripts/Systems/Gameplay/GameManagerSystem.cs
+++ Foxglove/Assets/Scripts/Systems/Gameplay/GameManagerSystem.cs
@@ -1,19 +1,30 @@
-﻿using Foxglove.Core.State;
+﻿using System;
+using Foxglove.Character;
+using Foxglove.Core.State;
 using Foxglove.Maps;
 using SideFX.Events;
 using Unity.Entities;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
 
 namespace Foxglove.Gameplay {
-    internal enum GameState { Startup, WaitForMap, MapReady }
+    internal enum GameState {
+        Startup,
+        WaitForMap,
+        MapReady,
+        Playing,
+    }
 
     internal sealed partial class GameManagerSystem : SystemBase, IStateMachineSystem<GameState> {
         private bool _mapIsReady;
         private EventBinding<MapReadyEvent> _mapReadyBinding;
+        private Random _rng;
 
         protected override void OnCreate() {
+            StateMachine.Init(CheckedStateRef, GameState.Startup);
+            _rng = new Random((uint)DateTimeOffset.UtcNow.GetHashCode());
             _mapReadyBinding = new EventBinding<MapReadyEvent>(OnMapReady);
             EventBus<MapReadyEvent>.Register(_mapReadyBinding);
-            StateMachine.Init(CheckedStateRef, GameState.Startup);
         }
 
         protected override void OnDestroy() {
@@ -42,6 +53,18 @@ namespace Foxglove.Gameplay {
             }
         }
 
+        private void SpawnPlayer() {
+            Entity mapEntity = SystemAPI.GetSingletonEntity<Map>();
+            DynamicBuffer<Room> rooms = SystemAPI.GetBuffer<Room>(mapEntity);
+            int roomIndex = _rng.NextInt() % rooms.Length;
+            Room room = rooms[roomIndex];
+            var spawnRequest = new SpawnCharacterEvent {
+                Character = SpawnableCharacter.Player,
+                Position = new float3(room.Center.x, 0.1f, room.Center.y),
+            };
+            EventBus<SpawnCharacterEvent>.Raise(spawnRequest);
+        }
+
         private void OnMapReady() {
             if (StateMachine.GetState<GameState>(CheckedStateRef).Current is GameState.WaitForMap)
                 StateMachine.SetNextState(CheckedStateRef, GameState.MapReady);
@@ -56,6 +79,12 @@ namespace Foxglove.Gameplay {
                     StateMachine.SetNextState(CheckedStateRef, GameState.WaitForMap);
                     return;
                 case GameState.WaitForMap:
+                    return;
+                case GameState.MapReady:
+                    SpawnPlayer();
+                    StateMachine.SetNextState(CheckedStateRef, GameState.Playing);
+                    return;
+                case GameState.Playing:
                     return;
                 default:
                     return;
