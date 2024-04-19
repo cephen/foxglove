0f753af2adf28b7902a1d1cf3f383684f195a025
Ash Villaschi <4luxxx@gmail.com>
2024-04-19 16:08:00 +0100

    pause menu exit button loads main menu

--- Foxglove/Assets/Scripts/MonoBehaviours/PauseMenuController.cs
+++ Foxglove/Assets/Scripts/MonoBehaviours/PauseMenuController.cs
@@ -1,11 +1,14 @@
 ﻿using Foxglove.Gameplay;
 using SideFX.Events;
+using SideFX.SceneManagement;
+using SideFX.SceneManagement.Events;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Foxglove {
     [RequireComponent(typeof(UIDocument))]
     public sealed class PauseMenuController : MonoBehaviour {
+        [SerializeField] private MainMenuScene _mainMenuScene;
         private UIDocument _doc;
         private Button _resumeButton;
         private Button _exitButton;
@@ -21,18 +24,19 @@ namespace Foxglove {
         private void OnEnable() {
             EventBus<PauseEvent>.Register(_pauseBinding);
             _resumeButton.clicked += OnResumeClicked;
+            _exitButton.clicked += OnExitClicked;
         }
 
         private void OnDisable() {
             EventBus<PauseEvent>.Deregister(_pauseBinding);
             _resumeButton.clicked -= OnResumeClicked;
+            _exitButton.clicked -= OnExitClicked;
         }
 
-        private void OnResumeClicked() {
-            _doc.enabled = false;
-            EventBus<ResumeEvent>.Raise(new ResumeEvent());
-        }
+        private static void OnResumeClicked() => EventBus<ResumeEvent>.Raise(new ResumeEvent());
+        private void OnExitClicked() => EventBus<LoadRequest>.Raise(new LoadRequest(_mainMenuScene));
 
         private void OnPause() => _doc.enabled = true;
+        private void OnResume() => _doc.enabled = false;
     }
 }
