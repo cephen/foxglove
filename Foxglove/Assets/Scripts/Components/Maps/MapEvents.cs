fb1a1948adc9ef6734ce1bda3729904467c86995
Ash Villaschi <4luxxx@gmail.com>
2024-04-23 17:01:08 +0100

    broadcast spawn locations in map ready event

--- Foxglove/Assets/Scripts/Components/Maps/MapEvents.cs
+++ Foxglove/Assets/Scripts/Components/Maps/MapEvents.cs
@@ -1,7 +1,10 @@
 ﻿using SideFX.Events;
 
 namespace Foxglove.Maps {
-    public readonly struct MapReadyEvent : IEvent { }
+    public readonly struct MapReadyEvent : IEvent {
+        public readonly Room PlayerSpawnRoom { get; init; }
+        public readonly Room TeleporterSpawnRoom { get; init; }
+    }
 
     public readonly struct BuildMapEvent : IEvent { }
 
