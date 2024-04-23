c2e55078f61545a0016e50792beda0a2ddd6c4f4
Ash Villaschi <4luxxx@gmail.com>
2024-04-23 15:30:02 +0100

    Add a player died event

---
+++ Foxglove/Assets/Scripts/Components/Character/CharacterEvents.cs
@@ -0,0 +1,9 @@
+﻿using SideFX.Events;
+using Unity.Entities;
+
+namespace Foxglove.Character {
+    public readonly struct PlayerDied : IEvent {
+        public PlayerDied(Entity entity) => Entity = entity;
+        public Entity Entity { get; init; }
+    }
+}
