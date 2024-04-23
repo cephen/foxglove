0936081e8282039c65ebd28028effb2734a1f228
Ash Villaschi <4luxxx@gmail.com>
2024-04-23 15:29:53 +0100

    Add health regen to the player

---
+++ Foxglove/Assets/Scripts/Authoring/Character/HealthRegenAuthoring.cs
@@ -0,0 +1,16 @@
+﻿using Foxglove.Combat;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Foxglove.Authoring.Character {
+    internal sealed class HealthRegenAuthoring : MonoBehaviour {
+        public float RegenRate = 2.5f;
+
+        private sealed class Baker : Baker<HealthRegenAuthoring> {
+            public override void Bake(HealthRegenAuthoring authoring) {
+                Entity e = GetEntity(authoring.gameObject, TransformUsageFlags.None);
+                AddComponent(e, new HealthRegen(authoring.RegenRate));
+            }
+        }
+    }
+}
--- Foxglove/Assets/Scripts/Components/Combat/Health.cs
+++ Foxglove/Assets/Scripts/Components/Combat/Health.cs
@@ -4,18 +4,23 @@ namespace Foxglove.Combat {
     public struct Health : IComponentData {
         public float Current;
         public float Max;
+        public uint LastDamagedAt;
 
         public Health(float max) {
             Max = max;
             Current = max;
+            LastDamagedAt = 0;
         }
 
         public void Reset() {
             Current = Max;
+            LastDamagedAt = 0;
         }
     }
 
     public struct HealthRegen : IComponentData, IEnableableComponent {
         public float Rate;
+
+        public HealthRegen(float rate) => Rate = rate;
     }
 }
